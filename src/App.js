0a52a017f80ee8c1b68f871ab5087c7f286aa80b
Keiraz24 <113703994+Keiraz24@users.noreply.github.com>
2023-10-26 13:13:08 -0400

    third

--- src/App.js
+++ src/App.js
@@ -1,46 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function Square({value}){
-  function handleClick() {
-    console.log('clicked!');
-  }
-    return (
-      <button
-        className="square"
-        onClick={handleClick}
-      >
-        {value}
-      </button>
-    );
-
+function Square(){
+ return <button className="square">{value}</button>
+  
 }
   
   
 
-export default function Square() {
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
 return (
   <>
   <div className="board-row">
-    <Square value ="1"/>
-    <Square value ="2"/>
-    <Square value ="3"/>
+    <Square value={squares[0]}/>
+    <Square value={squares[1]}/>
+    <Square value={squares[2]}/>
     {/* <button className="square">1</button>
     <button className="square">2</button>
     <button className="square">3</button> */}
   </div>
   <div className="board-row">
-  <Square value ="4"/>
-  <Square value ="5"/>
-  <Square value ="6"/>
+  <Square value={squares[3]}/>
+  <Square value={squares[4]}/>
+  <Square value={squares[5]}/>
     {/* <button className="square">4</button>
     <button className="square">5</button>
     <button className="square">6</button> */}
   </div>
   <div className="board-row">
-  <Square value ="7"/>
-  <Square value ="8"/>
-  <Square value ="9"/>
+  <Square value={squares[6]}/>
+  <Square />
+  <Square />
     {/* <button className="square">7</button>
     <button className="square">8</button>
     <button className="square">9</button> */}
