aaea0d75c98b8bf5546ef9b435c96a6e32447e8f
Keiraz24 <113703994+Keiraz24@users.noreply.github.com>
2023-10-31 21:26:41 -0400

    more

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-function Square(){
- return <button className="square">{value}</button>
+function Square({value, onSquareClick}){
+ return <button className="square" onClick={onSquareClick}>{value}</button>
   
 }
   
@@ -11,28 +11,34 @@ function Square(){
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
 return (
   <>
   <div className="board-row">
-    <Square value={squares[0]}/>
-    <Square value={squares[1]}/>
-    <Square value={squares[2]}/>
+    <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+    <Square value={squares[1]}  onSquareClick={() => handleClick(1)}/>
+    <Square value={squares[2]}  onSquareClick={() => handleClick(2)}/>
     {/* <button className="square">1</button>
     <button className="square">2</button>
     <button className="square">3</button> */}
   </div>
   <div className="board-row">
-  <Square value={squares[3]}/>
-  <Square value={squares[4]}/>
-  <Square value={squares[5]}/>
+  <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+  <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+  <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
     {/* <button className="square">4</button>
     <button className="square">5</button>
     <button className="square">6</button> */}
   </div>
   <div className="board-row">
-  <Square value={squares[6]}/>
-  <Square />
-  <Square />
+  <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+  <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+  <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     {/* <button className="square">7</button>
     <button className="square">8</button>
     <button className="square">9</button> */}
